ea2d8fea44f0516679d40cdc177ddf8eaa3ff137
euphemia <euphyfzhang@gmail.com>
2023-09-22 22:17:51 -0400

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,15 +28,13 @@ st.header('Frosty LLM Chatbot', divider = 'rainbow')
 conn = st.experimental_connection("snowpark")
 df = conn.query("select primary_type from TORONTO_CRIME_DB.RAW.SUMMARY_CRIME_COUNTS;", ttl = 600)
 
-itext = df.iloc[0].values[0]
-options = st.multiselect('Select the primary type(s)', itext)
-	st.dataframe(
-    df,
-    column_config={
-        "primary_type": "Crime Primary Type",
-    },
-    hide_index=True,
-	)
+st.dataframe(
+df,
+column_config={
+	"primary_type": "Crime Primary Type",
+},
+hide_index=True,
+)
 
 """
 itext = f'What is the meaning of {df.iloc[0].values[0]} in term of crime?'
