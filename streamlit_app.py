15b30e4640b94954d4f719cf84ef2c1401c5b3fb
euphemia <euphyfzhang@gmail.com>
2023-09-22 20:58:01 -0400

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,20 @@ def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
     st.info(llm(input_text))
 
-st.header('Frosty LLM Chatbot', divider = 'rainbow')
-
-conn = st.experimental_connection("snowpark")
-df = conn.query("select distinct primary_type from TORONTO_CRIME_DB.RAW.CHICAGO_CRIMES LIMIT 1;", ttl = 600)
-
-
 with st.form('my_form'):
-	text = f'What is the meaning of {df.iloc[0].values[0]}?'
-    text = st.text_area(text)
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
     submitted = st.form_submit_button('Submit')
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
         generate_response(text)
 		
+
+st.header('Frosty LLM Chatbot', divider = 'rainbow')
+
+conn = st.experimental_connection("snowpark")
+df = conn.query("select distinct primary_type from TORONTO_CRIME_DB.RAW.CHICAGO_CRIMES LIMIT 3;", ttl = 600)
+
+
+itext = f'What is the meaning of {df.iloc[0].values[0]}?'
+generate_response(itext)
